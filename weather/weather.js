c4ed140233a5a00b0e9a459adadc8b8948e5f650
Jacob Finn <super.aardvark@gmail.com>
2018-03-18 00:27:32 -0700

    Added Eureka weather; 'Any' selected after changing zones

--- weather/weather.js
+++ weather/weather.js
@@ -86,7 +86,8 @@ weatherChances: {
 "Kugane": function(chance) { if ((chance -= 10) < 0) { return "Rain"; } else if ((chance -= 10) < 0) { return "Fog"; } else if ((chance -= 20) < 0) { return "Clouds"; } else if ((chance -= 40) < 0) { return "Fair Skies"; } else { return "Clear Skies"; } },
 "The Ruby Sea": function(chance) { if ((chance -= 10) < 0) { return "Thunder"; } else if ((chance -= 10) < 0) { return "Wind"; } else if ((chance -= 15) < 0) { return "Clouds"; } else if ((chance -= 40) < 0) { return "Fair Skies"; } else { return "Clear Skies"; } },
 "Yanxia": function(chance) { if ((chance -= 5) < 0) { return "Showers"; } else if ((chance -= 10) < 0) { return "Rain"; } else if ((chance -= 10) < 0) { return "Fog"; } else if ((chance -= 15) < 0) { return "Clouds"; } else if ((chance -= 40) < 0) { return "Fair Skies"; } else { return "Clear Skies"; } },
-"The Azim Steppe": function(chance) { if ((chance -= 5) < 0) { return "Gales"; } else if ((chance -= 5) < 0) { return "Wind"; } else if ((chance -= 7) < 0) { return "Rain"; } else if ((chance -= 8) < 0) { return "Fog"; } else if ((chance -= 10) < 0) { return "Clouds"; } else if ((chance -= 40) < 0) { return "Fair Skies"; } else { return "Clear Skies"; } }
+"The Azim Steppe": function(chance) { if ((chance -= 5) < 0) { return "Gales"; } else if ((chance -= 5) < 0) { return "Wind"; } else if ((chance -= 7) < 0) { return "Rain"; } else if ((chance -= 8) < 0) { return "Fog"; } else if ((chance -= 10) < 0) { return "Clouds"; } else if ((chance -= 40) < 0) { return "Fair Skies"; } else { return "Clear Skies"; } },
+"Eureka": function(chance) { if ((chance -= 10) < 0) { return "Snow"; } else if ((chance -= 30) < 0) { return "Showers"; } else if ((chance -= 30) < 0) { return "Gales"; } else { return "Fair Skies"; } }
 },
 
 weatherLists: {
@@ -128,6 +129,7 @@ weatherLists: {
 "Kugane": ["Rain","Fog","Clouds","Fair Skies","Clear Skies"],
 "The Ruby Sea": ["Thunder","Wind","Clouds","Fair Skies","Clear Skies"],
 "Yanxia": ["Showers","Rain","Fog","Clouds","Fair Skies","Clear Skies"],
-"The Azim Steppe": ["Gales","Wind","Rain","Fog","Clouds","Fair Skies","Clear Skies"]
+"The Azim Steppe": ["Gales","Wind","Rain","Fog","Clouds","Fair Skies","Clear Skies"],
+"Eureka": ["Fair Skies", "Gales", "Showers", "Snow"]
 }
 };
